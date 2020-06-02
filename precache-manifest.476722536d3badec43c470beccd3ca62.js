ca2bf437b97cdb01b1cc25c03bc38a37a68b7450
Shreya Varadaraj <svara001@odu.edu>
2020-06-01 21:12:33 -0400

    Updates

---
+++ precache-manifest.476722536d3badec43c470beccd3ca62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98187415cfbb8dface8c22d6017d3fca",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "c2c8b78bb9f78d57c6ee",
+    "url": "/To-Do-List/static/css/main.2376f690.chunk.css"
+  },
+  {
+    "revision": "224a134b5add063632d6",
+    "url": "/To-Do-List/static/js/2.a3c0a190.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List/static/js/2.a3c0a190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2c8b78bb9f78d57c6ee",
+    "url": "/To-Do-List/static/js/main.f9662600.chunk.js"
+  },
+  {
+    "revision": "1a257e002a8e61775ae7",
+    "url": "/To-Do-List/static/js/runtime-main.66e56b41.js"
+  }
+]);
